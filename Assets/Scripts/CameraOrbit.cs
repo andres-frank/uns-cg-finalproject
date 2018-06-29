87c24098406e6a1480e5ef4fe0544b44e4fa853b
Gabriel SG <gsalazargisbert@gmail.com>
2018-06-28 22:23:00 -0300

    Better Control of the camera

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -21,27 +21,26 @@ public class CameraOrbit : MonoBehaviour
     public bool AutoRotation = true;
     public float AutoRotateX = 0.2f;
     public float AutoRotateY = 0.1f;
+    private bool cameraEnable = true;
  
  
     // Use this for initialization
     void Start() {
         this._XForm_Camera = this.transform;
         this._XForm_Parent = this.transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
     }
  
  
     void LateUpdate() {
- 
- 		if (AutoRotation) {
- 			_LocalRotation.x += Time.deltaTime + AutoRotateX;
- 			//_LocalRotation.y += Time.deltaTime + AutoRotateY;
- 		}
-       
-        //Rotation of the Camera based on Mouse Coordinates
-        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0) {
-            _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
-            _LocalRotation.y += Input.GetAxis("Mouse Y") * MouseSensitivity;
+        
+        if (AutoRotation) {
+            _LocalRotation.x += Time.deltaTime + AutoRotateX;
+            //_LocalRotation.y += Time.deltaTime + AutoRotateY;
         }
+    
         //Zooming Input from our Mouse Scroll Wheel
         if (Input.GetAxis("Mouse ScrollWheel") != 0f) {
             float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitvity;
@@ -52,22 +51,43 @@ public class CameraOrbit : MonoBehaviour
 
             this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 100f);
         }
-        
- 		//Clamp the y Rotation to horizon and not flipping over at the top
- 		_LocalRotation.y = Mathf.Clamp(_LocalRotation.y, 0f, 90f);
 
+        if(Input.GetMouseButtonDown(1)){ 
+            toggleCursorVisibility();
+            cameraEnable = !cameraEnable;
 
-        //Actual Camera Rig Transformations
-        Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
-        this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
- 
-        if ( this._XForm_Camera.localPosition.z != this._CameraDistance * -1f )
-        {
-            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
         }
+        if(cameraEnable){
+            //Rotation of the Camera based on Mouse Coordinates
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0) {
+                _LocalRotation.x -= Input.GetAxis("Mouse X") * MouseSensitivity;
+                _LocalRotation.y += Input.GetAxis("Mouse Y") * MouseSensitivity;
+            }
+            
+            //Clamp the y Rotation to horizon and not flipping over at the top
+            _LocalRotation.y = Mathf.Clamp(_LocalRotation.y, -90f, 90f);
+        }
+            //Actual Camera Rig Transformations
+            Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
+            this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
+    
+            if ( this._XForm_Camera.localPosition.z != this._CameraDistance * -1f ){
+                this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
+            }
     }
 
     public void toggleAutoRotation() {
         this.AutoRotation = !AutoRotation;
+    }
+
+    public void toggleCursorVisibility(){
+        if(Cursor.lockState.Equals(CursorLockMode.None)){
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else{
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
